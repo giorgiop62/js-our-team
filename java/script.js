1af12782d38de8af9b068d3c47af94443b5eedd2
giorgip62 <giorgiop62@gmail.com>
2022-10-25 20:42:23 +0200

    creata la struttura

---
+++ java/script.js
@@ -0,0 +1,43 @@
+const row = document.querySelector(".row");
+
+const profilo = [
+  {
+    name:"Wayne Barnett",
+    role:"Founder & CEO",
+    immagine:"./img/wayne-barnett-founder-ceo.jpg",
+
+},
+{
+  name:"Angela Caroll",
+  role:"chief Editor",
+  immagine:"./img/angela-caroll-chief-editor.jpg"
+
+},
+
+{
+  name:"Walter Gordon",
+  role:"officine Manager",
+  immagine:"./img/walter-gordon-office-menager.jpg"
+
+},
+
+{
+  name:"Angela Lopez",
+  role:"Social Media Manager",
+  immagine:"./img/jpg"
+},
+
+{
+  name:"Scott Exstrada",
+  role:"Developer",
+  immagine:"./img/scott-estrada-developer.jpg"
+
+},
+
+{
+  name:"Barbara Ramos",
+  role:"Graphic Designer",
+  immagine: "./img/barbara-ramos-graphic-designer.jpg"
+}
+];
+
